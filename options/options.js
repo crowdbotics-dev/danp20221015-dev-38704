d013a8224c1f80358381ea3eecd4118ff690ab94
Daniel Palacios <daniel80380@gmail.com>
2022-10-14 11:40:01 -0300

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,10 +1,45 @@
+const styles = StyleSheet.create({
+  "hr": {
+    "marginTop": 20,
+    "marginBottom": 20,
+    "borderBottomColor": "white",
+    "borderBottomWidth": 1
+  },
+  "container": {
+    "flex": 1,
+    "height": 100,
+    "padding": 13
+  },
+  "text": {
+    "color": "black",
+    "fontSize": 20
+  },
+  "buttonPressed": {
+    "backgroundColor": "aquamarine"
+  },
+  "buttonNotPressed": {
+    "backgroundColor": "blue"
+  },
+  "button": {
+    "borderRadius": 4,
+    "padding": 15,
+    "marginTop": 10
+  },
+  "buttonText": {
+    "color": "white",
+    "textAlign": "center",
+    "fontSize": 16
+  }
+});
 export const globalOptions = {
-  name: "danp20221015_dev_38704",
-  url: "https://danp20221015_dev_38704.botics.co",
-  api: "https://danp20221015_dev_38704.botics.co/api/v1"
+  "name": "danp20221015_dev_38704",
+  "url": "https://danp20221015_dev_38704.botics.co",
+  "api": "https://danp20221015_dev_38704.botics.co/api/v1"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
-    "copy": "Routes available!"
+    "title": "App66",
+    "copy": "Ro666",
+    "styles": styles
   }
 };
